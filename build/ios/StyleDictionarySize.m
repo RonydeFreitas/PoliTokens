583f3181baef45e4d0d7dbf86ff157a3c4894acd
Rony de Freitas <rony.fre@gmail.com>
2022-11-24 10:57:17 -0300

    Criação dos tokens

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Thu, 24 Nov 2022 13:49:46 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
