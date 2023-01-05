ab1e84f5309ae8ebe089ff9a84276cc5d1e7325f
Rony de Freitas <rony.fre@gmail.com>
2023-01-05 08:36:36 -0300

    Update

---
+++ build/js/_fonts.js
@@ -0,0 +1,30 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 05 Jan 2023 10:46:16 GMT
+ */
+
+module.exports = {
+  "contentNormal": "16 * 1",
+  "contentSm": "16 * 0.750",
+  "contentXs": "16 * 0.625",
+  "titleH6": "16 * 1.125",
+  "titleH5": "16 * 1.25",
+  "titleH4": "16 * 1.375",
+  "titleH3": "16 * 1.50",
+  "titleH2": "16 * 1.75",
+  "titleH1": "16 * 2",
+  "lineHeightsH1": "16 * 2.5",
+  "lineHeightsH2": "16 * 2.25",
+  "lineHeightsH3": "16 * 2.25",
+  "lineHeightsH4": "16 * 1.75",
+  "lineHeightsH5": "16 * 1.5",
+  "lineHeightsH6": "16 * 1.5",
+  "lineHeightsNormal": "16 * 1.5",
+  "lineHeightsSm": "16 * 1",
+  "lineHeightsXs": "16 * 0.875",
+  "textBold": "700",
+  "semiBold": "600",
+  "normal": "400",
+  "base": "16",
+  "fonth1": "16 * 2.5"
+};
